aa96d6a1dcbe2c0d17cdeab6e87fce5b18fefaad
GolDHoon <128574880+GolDHoon@users.noreply.github.com>
2023-03-31 19:51:22 +0900

    소스 업데이트

--- source/test.java
+++ source/test.java
@@ -1,9 +1,17 @@
 import java.time.LocalDateTime;
 
 public class test{
+
+}
+
+class className{
 public void function(){
     LocalDateTime now = LocalDateTime.now();
     
     now.withHour(00);
 }
+}
+
+interface interfaceName<T>{
+    T ifTest();
 }
